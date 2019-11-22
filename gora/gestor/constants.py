9921561cb6dce482c4a76444dbe3890b0502b961
Gonzalo Ruiz <gonzalo.ruiz@avature.net>
2019-11-22 17:28:39 -0300

    eeeaaae

---
+++ gora/gestor/constants.py
@@ -0,0 +1,11 @@
+#VIEWS
+ORDER_ADD = "order_add.html"
+ORDER_INDEX = "index.html"
+ORDER_DETAIL = "order_detail.html"
+
+#MODELS
+
+#Orders
+ORDER_STATE_SUCCESS = "En"
+ORDER_STATE_SUCCESS_TEXT = "Entregado"
+        
